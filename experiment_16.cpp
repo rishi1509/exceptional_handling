896cc3482f06143a5df23cc4599020c8373e5f28
rishi1509 <139452304+rishi1509@users.noreply.github.com>
2023-10-06 09:19:06 +0530

    Update experiment_16.cpp

--- experiment_16.cpp
+++ experiment_16.cpp
@@ -1,3 +1,5 @@
+/*PRN: 22070123095
+Experiment-no: 16(a)*/
 #include<iostream>
 using namespace std;
 int main()
@@ -29,3 +31,43 @@ int main()
     return 0;
     
 }
+/*OUTPUT-enter 2 numbers:10 0 
+Divide by 0 error
+*/
+
+/*PRN: 22070123095
+Experiment-no: 16(b)*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"\nenter your age:";
+    cin>>a;
+    try
+    {
+        if(a>=18)
+        {
+           cout<<"you are adult";
+            
+        }
+        else
+        {
+            throw a;
+            
+        }
+        
+    }
+    catch(int e)
+    {
+        cout<<"\nyour age is " << e << " error";
+        
+        
+    }
+    return 0;
+    
+}
+/*OUTPUT-enter your age:10
+
+your age is 10 error*/
